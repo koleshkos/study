c6424932786b73e7d46ab2d8450a307a22f6e434
koleshkos <koleshkoser@gmail.com>
2017-03-12 19:07:51 +0300

    Create z3.pas

---
+++ pascall_beginers/z3.pas
@@ -0,0 +1,17 @@
+var a:real;
+i, k:integer;
+begin
+ assign(input, 'input.txt');
+ reset(input);
+ assign(output, 'output.txt');
+ rewrite(output);
+
+ for i:=1 to 10 do begin
+	read(a);
+	k:=0;
+	if abs(a)>7 then inc(k);	
+ end;
+ writeln(k); 
+ close(input);
+ close(output);
+end.
