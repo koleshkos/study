9d1b5137e41abf5c1462fbe9da9982dd0a9cb7ea
koleshkos <koleshkoser@gmail.com>
2017-03-12 19:10:06 +0300

    Update z3.pas

--- pascall_beginers/z3.pas
+++ pascall_beginers/z3.pas
@@ -1,17 +1,28 @@
-var a:real;
-i, k:integer;
+var
+ a, i:integer;
 begin
  assign(input, 'input.txt');
  reset(input);
  assign(output, 'output.txt');
  rewrite(output);
+ 
+ procedure return;
+ begin
+	writeln('да');
+	close(input);
+    close(output);
+	halt;
+ end;
 
  for i:=1 to 10 do begin
 	read(a);
-	k:=0;
-	if abs(a)>7 then inc(k);	
+	if a=7 then begin
+	  writeln('yes');
+	  exit;
+	 end;
  end;
- writeln(k); 
+ writeln('no');
+
  close(input);
- close(output);
+ close(output); 
 end.
