b608a1857b7c5dd9c069bca94cdce5f931320145
koleshkos <koleshkoser@gmail.com>
2017-03-16 23:18:52 +0300

    Update z10.pas

--- pascall_beginers/z10.pas
+++ pascall_beginers/z10.pas
@@ -8,12 +8,13 @@ begin
  assign(output, 'output.txt');
  rewrite(output);
 
- for i:=1 to 5 do
-  for j:=1 to 5 do begin 
-	read(a);
-	a:=-a;
-	write(a,' ');
-   end;
+ for i:=1 to 5 do begin
+   for j:=1 to 5 do begin 
+	 read(a);
+	 write(-a,' ');
+    end;
+   writeln;
+  end; 
    
  close(input);
  close(output); 
